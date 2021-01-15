a07b1c74c7cca76b70fbcd75728db030c81020e9
Sahil Athia <sahilathia918@gmail.com>
2021-01-15 22:39:52 +0000

    update cors and add axios request for data

--- client/src/components/organization/Jobs.jsx
+++ client/src/components/organization/Jobs.jsx
@@ -1,6 +1,20 @@
 import {Link} from 'react-router-dom'
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 export default function Jobs(props) {
+  const [state, setState] = useState({jobs: {}})
+
+  useEffect(() => {
+    Promise.all([
+      axios.get("http://localhost:8080/api/v1/job"),
+    ])
+    .then(all => {
+      setState(prev => ({...prev, jobs: all[0].data.data}))
+    })
+    .catch(e => console.log(e));
+  }, [])
+  console.log(state)
   return (
     <>
       <h1>This is the Jobs component</h1>
