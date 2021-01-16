51216cc456e147a50640ef3c08e87a18e4c4126c
Sahil Athia <sahilathia918@gmail.com>
2021-01-16 20:58:57 +0000

    add login functions into frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,17 +12,37 @@ import Home from "./components/Home"
 import Main from "./components/Main"
 import Organization from "./components/Organization"
 import Individual from "./components/Individual"
+import {useEffect, useState} from 'react'
 
 function App() {
-  const user_type = "BUISNESS"
-    const example = 4
-    axios.get(`/api/v1/user/${example}`)
-    .then((res) => {
-      console.log(res.data);
-    }).catch((err) => {
-      console.log(err);
-    });
+  const [state, setState] = useState({isLoggedIn: false, user: {}})
 
+  const handleLogin = (data) => {
+    this.setState({
+      isLoggedIn: true,
+      user: data.user
+    })
+  }
+  const handleLogout = () => {
+    this.setState({
+    isLoggedIn: false,
+    user: {}
+    })
+  }
+
+  loginStatus = () => {
+    axios
+      .get('http://localhost:8080/logged_in', 
+              {withCredentials: true})    
+      .then(response => {
+        if (response.data.logged_in) {
+          handleLogin(response)
+        } else {
+          handleLogout()
+        }
+      })
+      .catch(error => console.log('api errors:', error))
+  };
 
   return (
     <Router>
@@ -56,6 +76,8 @@ function App() {
             <Organization />
           </Route>
 
+          <Route exact path='/login' component={}/>
+          <Route exact path='/signup' component={}/>
           
         </Switch>
       </div>
@@ -65,3 +87,13 @@ function App() {
 
 
 export default App;
+
+
+// const user_type = "BUISNESS"
+//     const example = 4
+//     axios.get(`/api/v1/user/${example}`)
+//     .then((res) => {
+//       console.log(res.data);
+//     }).catch((err) => {
+//       console.log(err);
+//     });
