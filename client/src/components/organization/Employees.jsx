8eb555b88d553c04ac302722b4678fafd7b81b93
Yu Gao <gaoyububest@gmail.com>
2021-01-16 20:00:36 +0000

    Show users data on Employee component

--- client/src/components/organization/Employees.jsx
+++ client/src/components/organization/Employees.jsx
@@ -1,9 +1,45 @@
-// import {Link} from 'react-router-dom'
+const Employees = ({employees}) => {
 
-export default function Employees(props) {
+  const organizationEmployees = employees.map((employee) => (
+    <div>
+      <div>id: </div>
+      <div>{employee.id}</div>
+      <div>name: </div>
+      <div>{employee.name}</div>
+      <div>email: </div>
+      <div>{employee.email}</div>
+      <div>summary: </div>
+      <div>{employee.summary}</div>
+      <div>industry: </div>
+      <div>{employee.industry}</div>
+      <div>skills: </div>
+      <div>{employee.skills}</div>
+      <div>education: </div>
+      <div>{employee.education}</div>
+      <div>experience: </div>
+      <div>{employee.experience}</div>
+      <div>location: </div>
+      <div>{employee.location}</div>
+      <div>contact: </div>
+      <div>{employee.contact}</div>
+      <div>resume_url: </div>
+      <div>{employee.resume_url}</div>
+      <div>organization_id: </div>
+      <div>{employee.organization_id}</div>
+      <div>verified: </div>
+      <div>{employee.verified}</div>
+
+    </div>
+  ));
+
   return (
     <>
+      <article class='employees'>
       <h1>This is the Employees component</h1>
+        {organizationEmployees}
+      </article>
     </>
   )
-}
+};
+
+export default Employees;
