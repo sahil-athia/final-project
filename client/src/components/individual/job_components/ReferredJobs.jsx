f38edf585206835903dea004c317811723e5be27
Sahil Athia <sahilathia918@gmail.com>
2021-01-24 21:28:10 +0000

    add css to job boxes

--- client/src/components/individual/job_components/ReferredJobs.jsx
+++ client/src/components/individual/job_components/ReferredJobs.jsx
@@ -1,17 +1,22 @@
 import { React } from 'react';
 import AcceptButton from './AcceptButton';
+import './position.scss'
 
 const ReferredJobs = (props) => {
+
   const { referredJobs, handleAccept} = props;
+
   const jobList = referredJobs.map((job) => {
     const reference_id = Object.keys(job)[0];
     const {id, title, description, salary} = job[reference_id][0];
     const accepted = job["accepted"];
+    
     return (
-      <div key={id}>
-        <div>Title: {title}</div>
-        <div>Description: {description}</div>
-        <div>Salary: {salary}</div>
+      <div key={id} className="job-box" >
+        <img src="https://upload-icon.s3.us-east-2.amazonaws.com/uploads/icons/png/10625049341582994876-512.png" alt="job-icon" className="job-icon"></img>
+        <div className="job-title">{title}</div>
+        <div className="job-description"><em>{description}</em></div>
+        <div className="job-salary">{salary}</div>
         <AcceptButton
           reference_id={reference_id}
           handleAccept={handleAccept}
--- client/src/components/individual/job_components/ShowPosition.jsx
+++ client/src/components/individual/job_components/ShowPosition.jsx
@@ -1,18 +1,22 @@
 // import MakeReference from '../../icons/MakeReference.png';
+import './position.scss'
+
 
 const ShowPosition = ({job, onReference}) => {
   
   return (
-    <>
-      <div>Title: {job.title}</div>
-      <div>Description: {job.description}</div>
-      <div>Salary: {job.salary}</div>
-      <button onClick={onReference}>
-      {/* <img className="reference-btn" onClick={onReference} src={MakeReference} alt="Reference"/> */}
-        Refer Someone
+    <div className="job-box" >
+      <img src="https://upload-icon.s3.us-east-2.amazonaws.com/uploads/icons/png/10625049341582994876-512.png" alt="job-icon" className="job-icon"></img>
+      <div className="job-title"><b>{job.title}</b></div>
+      <div className="job-description"><em>{job.description}</em></div>
+      <div className="job-salary" ><strong>{job.salary}</strong></div>
+      <button onClick={onReference} className="refer-button">
+        Refer
       </button>
-    </>
+    </div>
   )
 };
 
 export default ShowPosition;
+
+{/* <img className="reference-btn" onClick={onReference} src={MakeReference} alt="Reference"/> */}
